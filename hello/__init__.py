88bc518994edca5f71461028be143223ad192d8a
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2020-11-07 18:39:08 +0200

    Hello test

---
+++ hello/__init__.py
@@ -0,0 +1,22 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """hello.cpp exists"""
+    check50.exists("hello.cpp")
+
+@check50.check(exists)
+def compiles():
+    """hello.cpp compiles"""
+    check50.run("g++ hello.cpp -lcrypt -lcs50 -lm -o hello").exit(0)
+
+@check50.check(compiles)
+def emma():
+    """responds to name Emma"""
+    check50.run("./hello").stdin("Emma").stdout("Emma").exit()
+
+@check50.check(compiles)
+def rodrigo():
+    """responds to name Rodrigo"""
+    check50.run("./hello").stdin("Rodrigo").stdout("Rodrigo").exit()
