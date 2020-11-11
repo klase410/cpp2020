19e0a1442c35194ddd76e5d04b263e03e30abf2d
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2020-11-11 13:01:55 +0200

    Readability file IO update

--- readability/__init__.py
+++ readability/__init__.py
@@ -4,6 +4,7 @@ import check50
 def exists():
     """readability.cpp exists"""
     check50.exists("readability.cpp")
+    check50.include("input1.txt", "input2.txt")
 
 @check50.check(exists)
 def compiles():
@@ -15,6 +16,15 @@ def single_sentence_fileRead():
     """handles single sentence with multiple words"""
     check50.run("./readability").stdin("input1.txt").exit(0)
     with open('output.txt') as out
-        rez = out1.read()
+        rez = out.read()
         if (rez != "Grade 7\n"):
             raise check50.Failure("Expected result: Grade 7\n, not" + str(rez))
+            
+@check50.check(compiles)
+def single_sentence_fileRead():
+    """handles punctuation within a single sentence"""
+    check50.run("./readability").stdin("input2.txt").exit(0)
+    with open('output.txt') as out
+        rez = out.read()
+        if (rez != "Grade 9\n"):
+            raise check50.Failure("Expected result: Grade 9\n, not" + str(rez))
