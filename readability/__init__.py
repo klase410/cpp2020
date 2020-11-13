98cca78397bff51cecc3ebf2906f2ecbe3a42222
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2020-11-13 14:36:42 +0200

    Readability file IO update

--- readability/__init__.py
+++ readability/__init__.py
@@ -28,4 +28,4 @@ def single_sentence_other_punctuation_fileRead():
             
 def check_output(out, expected_out):
     if (out.read() != expected_out.read()):
-        raise check50.Failure("Calculated result result does not match with expected result" + out.read())
+        raise check50.Failure("Calculated result result does not match with expected result" + out.read() + "Hello")
