4e97c2cd3d9ac865f6e44dc4599ea2a1013e048c
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2020-11-13 17:04:41 +0200

    Readability file IO update

--- readability/__init__.py
+++ readability/__init__.py
@@ -1,15 +1,11 @@
 import check50
 
-@check50.check()
-def output_file_exists():
-    """output.txt exists"""
-    check50.exists("output.txt")
-    check50.include("expected_output1.txt", "expected_output1.txt")
-
 @check50.check(output_file_exists)
 def exists():
     """readability.cpp exists"""
     check50.exists("readability.cpp")
+    check50.include("input1.txt", "input2.txt")
+    check50.include("expected_output1.txt", "expected_output2.txt")
 
 @check50.check(exists)
 def compiles():
@@ -20,6 +16,7 @@ def compiles():
 def single_sentence_fileRead():
     """handles single sentence with multiple words"""
     check50.run("./readability").stdin("input1.txt").exit(0)
+    check50.run("cat input1.txt").exit(0)
     check_output(open("output.txt"), open("expected_output1.txt"))
     
 @check50.check(compiles)
