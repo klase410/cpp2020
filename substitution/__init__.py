16acc461849664af5c0518c466641a6f94d83dbf
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2020-11-14 12:42:52 +0200

    Substitution file IO update

--- substitution/__init__.py
+++ substitution/__init__.py
@@ -24,6 +24,66 @@ def encrypt2():
     check50.run("./substitution ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("input2.txt").exit(0)
     check_output(open("output.txt").read(), open("expected_output2.txt").read())
 
+@check50.check(compiles)
+def encrypt3():
+    """encrypts "ABC" as "NJQ" using NJQSUYBRXMOPFTHZVAWCGILKED as key"""
+    check50.run("./substitution NJQSUYBRXMOPFTHZVAWCGILKED").stdin("input3.txt").exit(0)
+    check_output(open("output.txt").read(), open("expected_output3.txt").read())
+
+@check50.check(compiles)
+def encrypt4():
+    """encrypts "XyZ" as "KeD" using NJQSUYBRXMOPFTHZVAWCGILKED as key"""
+    check50.run("./substitution NJQSUYBRXMOPFTHZVAWCGILKED").stdin("input4.txt").exit(0)
+    check_output(open("output.txt").read(), open("expected_output4.txt").read())
+
+@check50.check(compiles)
+def encrypt5():
+    """encrypts "This is CS50" as "Cbah ah KH50" using YUKFRNLBAVMWZTEOGXHCIPJSQD as key"""
+    check50.run("./substitution YUKFRNLBAVMWZTEOGXHCIPJSQD").stdin("input5.txt").exit(0)
+    check_output(open("output.txt").read(), open("expected_output5.txt").read())
+
+@check50.check(compiles)
+def encrypt6():
+    """encrypts "This is CS50" as "Cbah ah KH50" using yukfrnlbavmwzteogxhcipjsqd as key"""
+    check50.run("./substitution yukfrnlbavmwzteogxhcipjsqd").stdin("input6.txt").exit(0)
+    check_output(open("output.txt").read(), open("expected_output6.txt").read())
+
+@check50.check(compiles)
+def encrypt7():
+    """encrypts "This is CS50" as "Cbah ah KH50" using YUKFRNLBAVMWZteogxhcipjsqd as key"""
+    check50.run("./substitution YUKFRNLBAVMWZteogxhcipjsqd").stdin("input7.txt").exit(0)
+    check_output(open("output.txt").read(), open("expected_output7.txt").read())
+
+@check50.check(compiles)
+def encrypt8():
+    """encrypts all alphabetic characters using DWUSXNPQKEGCZFJBTLYROHIAVM as key"""
+    check50.run("./substitution DWUSXNPQKEGCZFJBTLYROHIAVM").stdin("input8.txt").exit(0)
+    check_output(open("output.txt").read(), open("expected_output8.txt").read())
+
+@check50.check(compiles)
+def handles_no_argv():
+    """handles lack of key"""
+    check50.run("./substitution").exit(1)
+
+@check50.check(compiles)
+def handles_invalid_length():
+    """handles invalid key length"""
+    check50.run("./substitution QTXDGMKIPV").exit(1)
+
+@check50.check(compiles)
+def handles_invalid_key_chars():
+    """handles invalid characters in key"""
+    check50.run("./substitution ZWGKPMJRYISHFEXQON2DLUACVT").exit(1)
+
+@check50.check(compiles)
+def handles_duplicate_chars():
+    """handles duplicate characters in key"""
+    check50.run("./substitution YFDTSMPBVIEERGHWONUAKLQXCZ").exit(1)
+    
+@check50.check(compiles)
+def handles_multiple_duplicate_chars():
+    """handles multiple duplicate characters in key"""
+    check50.run("./substitution BBCCEFGHIJKLMNOPQRSTUVWXYZ").exit(1)
 
 def check_output(out, expected_out):
     if (out != expected_out):
